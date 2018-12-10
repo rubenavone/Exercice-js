b06039e5c2b9013f8dd948d631ca8c45941b6d68
rubenavone <navone_ruben@msn.com>
2018-12-10 15:03:26 +0100

    Ajout des deux exercice js

--- echaufe-05/js/main.js
+++ echaufe-05/js/main.js
@@ -1,11 +1,28 @@
 /* Un programme demande de saisir 10 notes ( une par une et de 0 à 20 ) et affiche la moyenne à la fin */
 
-let tableau = []
-let nombreDeNote = 2;
+let tableau = [];
+let nombreDeNote = 10;
+let moyenneGenerale = 0;
+let somme = 0;
 
 for (let i = 0; i < nombreDeNote; i++) {
-    tableau.push(Number(prompt("Entrez votre note")));
-    console.log(tableau[i]);
+
+    tableau.push(Number(prompt("Entrez votre note entre 0 et 10")));
+
+    if(tableau[i] > 10 || tableau[i] < 0){
+        console.log('votre saisis est eronner elle ne sera pas compter ...')
+    }else {
+        somme += tableau[i];
+    }
     
+
+    console.log(somme);
+
+    console.log(tableau[i]);
 }
 
+console.log("Nous allons calculer la moyenne generale de la classe !");
+
+moyenneGenerale = somme / nombreDeNote;
+
+console.log(moyenneGenerale);
---
+++ exercice1/js/main.js
@@ -0,0 +1,15 @@
+/*
+● Réaliser un calculatrice simple
+
+○ choisir une opération : + * / -
+
+○ choisir un 1er nombre
+
+○ choisir un 2eme nombre
+
+○ Afficher le résultat
+
+● Bonus (Pour ceux qui ont encore du temps devant eux..)
+
+○ améliorer l'UI/UX pour que cela ressemble à une vraie calculatrice */
+
